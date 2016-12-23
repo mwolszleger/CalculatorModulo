40e4cf8779e76029482ff829bc36b5c9cadefa35
kkryus <kamilkryus@gmail.com>
2016-12-23 15:44:31 +0100

    -upgraded appearance -added basic calculator functionality

--- page_handling.js
+++ page_handling.js
@@ -1,18 +1,41 @@
+var display_element;
+var modulo_element;
+var first_number = true;
+
+function initialize() 
+{
+    if (document.readyState == 'complete') 
+	{
+        display_element = document.getElementById("display");
+		modulo_element = document.getElementById("modulo");
+    }
+}
+
 function changePage(event)
   {
 		document.getElementsByClassName("active")[0].className = "";
 		event.target.parentElement.className = "active";
-		if(event.target.innerHTML == "Calculator")
-		{
-			document.getElementsByClassName("panel-heading")[0].innerHTML = "Modulo Calculator";
-			document.getElementsByClassName("panel-body")[0].innerHTML = "<div id=\"inputs\"><input type=\"text\" id=\"display\" value=\"0\"><input type=\"text\" maxlength=\"2\" id=\"modulo\" value=\"0\"></div>";
-		}
-		else if(event.target.innerHTML == "Game")
+
+		if(event.target.innerHTML == "Game")
 		{
 		}
 		else
 		{
 			document.getElementsByClassName("panel-heading")[0].innerHTML = "Assumptions:";
-			document.getElementsByClassName("panel-body")[0].innerHTML = "Kalkulator:<br>+<br>-<br>*<br>odwrotny<br>potęga<br>przeciwny<br>pole na podanie n<br>Clear<br>Backspace<br>Konkurs<br>";
+			document.getElementsByClassName("panel-body")[0].innerHTML = "Kalkulator:<br><font color=\"green\">+</font><br><font color=\"green\">-</font><br><font color=\"green\">*</font><br>odwrotny<br>potęga<br>przeciwny<br><font color=\"green\">pole na podanie n</font><br>Clear<br>Backspace<br><font color=\"green\">Konkurs</font><br>";
 		}
   }
+  
+function changeInput(ev)
+{
+	if(ev.target.innerHTML == "display:")
+	{
+		display_element.className = "input-focus";
+		modulo_element.className = "";
+	}
+	else
+	{
+		display_element.className = "";
+		modulo_element.className = "input-focus";
+	}
+}
